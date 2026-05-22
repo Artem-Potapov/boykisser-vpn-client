5b47394d5a816d70ef8151da3d889f4272ff506d
Artem-Potapov <potapov2350@gmail.com>
2026-05-21 19:04:45 -0500

    bump up version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.7rc1"
+        versionName = "0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
